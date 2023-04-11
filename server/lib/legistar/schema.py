62f5a4372fbdbf8cd46074b2de0383e19307da27
Dave <davepeck@gmail.com>
2023-04-11 15:24:22 -0700

    Generic table scraper for Legistar; add get-calendar CLI

--- server/lib/legistar/schema.py
+++ server/lib/legistar/schema.py
@@ -9,6 +9,20 @@ from pydantic import Field, validator
 class BaseSchema(PydanticBase):
     """Base schema type for all Legistar-returned data."""
 
+    # CONSIDER:
+
+    # def dict(self, *args, **kwargs):
+    #     """Override dict() to ensure we remove None values from the output."""
+    #     # Keep exclude_none=False if explicitly provided, though.
+    #     final_kwargs = {"exclude_none": True, **kwargs}
+    #     return super().dict(*args, **final_kwargs)
+
+    # def json(self, *args, **kwargs):
+    #     """Override json() to ensure we remove None values from the output."""
+    #     # Keep exclude_none=False if explicitly provided, though.
+    #     final_kwargs = {"exclude_none": True, **kwargs}
+    #     return super().json(*args, **final_kwargs)
+
     class Config:
         allow_population_by_field_name = True
 
--- server/lib/legistar/scraper.py
+++ server/lib/legistar/scraper.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import datetime
 import typing as t
 import urllib.parse
@@ -8,21 +10,7 @@ from bs4 import BeautifulSoup, Tag
 from .errors import LegistarError
 from .schema import CalendarEntry
 
-
-class CalendarColumn:
-    NAME = 0
-    MEETING_DATE = 1
-    MEETING_CALENDAR = 2
-    MEETING_TIME = 3
-    MEETING_LOCATION = 4
-    MEETING_DETAILS = 5
-    AGENDA = 6
-    AGENDA_PACKET = 7
-    MINUTES = 8
-    SEATTLE_CHANNEL = 9
-
-
-CALENDAR_EXPECTED_COLUMNS = [
+CALENDAR_HEADERS = [
     "name",
     "meeting date",
     "",  # the ics icon
@@ -34,6 +22,171 @@ CALENDAR_EXPECTED_COLUMNS = [
     "minutes",
     "seattle channel",
 ]
+
+
+def clean_text(text: str) -> str:
+    """Clean up text from the Legistar website."""
+    return text.replace("\xa0", " ").strip()
+
+
+class RowScraper:
+    """
+    Utilities to cull structured data from an arbitrary Legistar website table row.
+    """
+
+    table_scraper: TableScraper
+    row: Tag
+
+    def __init__(self, table_scraper: TableScraper, row: Tag):
+        self.table_scraper = table_scraper
+        self.row = row
+
+    def get_text(self, header: str) -> str:
+        """Get the text of the cell in the given column."""
+        maybe_text = self.row.find_all("td")[
+            self.table_scraper.get_header_index(header)
+        ].text
+        if not isinstance(maybe_text, str):
+            raise LegistarError(f"Could not find text for header {header}")
+        return clean_text(maybe_text)
+
+    def get_optional_text(self, header: str) -> str | None:
+        """Get the text of the cell in the given column, if it exists."""
+        maybe_text = self.row.find_all("td")[
+            self.table_scraper.get_header_index(header)
+        ].text
+        if not isinstance(maybe_text, str):
+            return None
+        return clean_text(maybe_text)
+
+    def get_date(self, header: str) -> datetime.date:
+        """Get the date of the cell in the given column."""
+        text = self.get_text(header)
+        try:
+            return datetime.datetime.strptime(text, "%m/%d/%Y").date()
+        except ValueError as e:
+            raise LegistarError(f"Could not parse date {text}") from e
+
+    def get_optional_date(self, header: str) -> datetime.date | None:
+        """Get the date of the cell in the given column, if it exists."""
+        text = self.get_optional_text(header)
+        if text is None:
+            return None
+        try:
+            return datetime.datetime.strptime(text, "%m/%d/%Y").date()
+        except ValueError as e:
+            raise LegistarError(f"Could not parse date {text}") from e
+
+    def get_time(self, header: str) -> datetime.time:
+        """Get the time of the cell in the given column."""
+        text = self.get_text(header)
+        try:
+            return datetime.datetime.strptime(text, "%I:%M %p").time()
+        except ValueError as e:
+            raise LegistarError(f"Could not parse time {text}") from e
+
+    def get_optional_time(self, header: str) -> datetime.time | None:
+        """Get the time of the cell in the given column, if it exists."""
+        text = self.get_optional_text(header)
+        if text is None:
+            return None
+        try:
+            return datetime.datetime.strptime(text, "%I:%M %p").time()
+        except ValueError as e:
+            if text.lower() == "canceled":
+                return None
+            raise LegistarError(f"Could not parse time {text}") from e
+
+    def get_link_and_text(self, header: str) -> tuple[str, str]:
+        """Get the link and text of the cell in the given column."""
+        maybe_link = self.row.find_all("td")[
+            self.table_scraper.get_header_index(header)
+        ].find("a")
+        if not isinstance(maybe_link, Tag):
+            raise LegistarError(f"Could not find link for header {header}")
+        href = maybe_link.get("href")
+        if not isinstance(href, str):
+            raise LegistarError(f"Could not find href for header {header}")
+        absolute_href = urllib.parse.urljoin(self.table_scraper.scraper.base_url, href)
+        return clean_text(maybe_link.text), absolute_href
+
+    def get_optional_link_and_text(
+        self, header: str
+    ) -> tuple[str, str] | tuple[None, None]:
+        """Get the link and text of the cell in the given column, if it exists."""
+        maybe_link = self.row.find_all("td")[
+            self.table_scraper.get_header_index(header)
+        ].find("a")
+        if not isinstance(maybe_link, Tag):
+            return None, None
+        href = maybe_link.get("href")
+        if not isinstance(href, str):
+            return None, None
+        absolute_href = urllib.parse.urljoin(self.table_scraper.scraper.base_url, href)
+        return clean_text(maybe_link.text), absolute_href
+
+
+class TableScraper:
+    """
+    Utilities to cull structured data from an arbitrary Legistar website table.
+    """
+
+    scraper: LegistarScraper
+    table: Tag
+    headers: list[str]
+    _indexes: dict[str, int]
+
+    def __init__(
+        self,
+        scraper: LegistarScraper,
+        table: Tag,
+        header_class: str = "rgHeader",
+        row_class: str = "rgRow",
+    ):
+        self.scraper = scraper
+        self.table = table
+        self.headers = self._build_headers(header_class)
+        self._indexes = {header: i for i, header in enumerate(self.headers)}
+
+    def _clean_header(self, header: str) -> str:
+        """Clean up a header string."""
+        return header.replace(":", "").lower().strip()
+
+    def _build_headers(self, header_class: str) -> list[str]:
+        """Populate the headers list."""
+        return [
+            self._clean_header(header.text)
+            for header in self.table.find_all("th", class_=header_class)
+        ]
+
+    def get_header_index(self, header: str) -> int:
+        """Get the index of the given header."""
+        maybe_index = self._indexes.get(self._clean_header(header))
+        if maybe_index is None:
+            raise LegistarError(f"Could not find header {header}")
+        return maybe_index
+
+    def __iter__(self) -> t.Iterator[RowScraper]:
+        """Iterate over the rows of the table."""
+        for row in self.table.find_all("tr", class_="rgRow"):
+            if not isinstance(row, Tag):
+                raise LegistarError(f"Invalid row: {row}")
+            yield RowScraper(self, row)
+
+    @classmethod
+    def from_soup(
+        cls,
+        scraper: LegistarScraper,
+        soup: BeautifulSoup,
+        table_class: str = "rgMasterTable",
+        header_class: str = "rgHeader",
+        row_class: str = "rgRow",
+    ) -> "TableScraper":
+        """Construct a TableScraper from a BeautifulSoup object."""
+        table = soup.find("table", class_=table_class)
+        if not isinstance(table, Tag):
+            raise LegistarError(f"Could not find table with class {table_class}")
+        return cls(scraper, table, header_class=header_class, row_class=row_class)
 
 
 class LegistarScraper:
@@ -69,103 +222,31 @@ class LegistarScraper:
         text = self._get(path, **queryparams)
         return BeautifulSoup(text)
 
-    def _clean_text(self, text: str) -> str:
-        return text.encode("ascii", errors="ignore").decode("ascii").strip()
-
-    def _cell_link(
-        self, cell: t.Any, required: bool = True
-    ) -> tuple[str, str] | tuple[None, None]:
-        if not isinstance(cell, Tag):
-            if required:
-                raise LegistarError(f"_cell_link: expected Tag, got {type(cell)}")
-            return (None, None)
-        link = cell.find("a")
-        if not isinstance(link, Tag):
-            if required:
-                raise LegistarError(f"_cell_link: expected Tag, got {type(link)}")
-            return (None, None)
-        href = link.get("href")
-        if not isinstance(href, str):
-            if required:
-                raise LegistarError(f"_cell_link: expected str, got {type(href)}")
-            return (None, None)
-        absolute_href = urllib.parse.urljoin(self.base_url, href)
-        text = self._clean_text(cell.text)
-        return (text, absolute_href)
-
-    def _cell_text(self, cell: t.Any, required: bool = True) -> str | None:
-        if not isinstance(cell, Tag):
-            if required:
-                raise LegistarError(f"_cell_text: expected Tag, got {type(cell)}")
-            return None
-        text = self._clean_text(cell.text)
-        return text
-
-    def _cell_date(self, cell: t.Any, required: bool = True) -> datetime.date | None:
-        text = self._cell_text(cell, required=required)
-        if text is None:
-            return None
-        try:
-            return datetime.datetime.strptime(text, "%m/%d/%Y").date()
-        except ValueError as e:
-            raise LegistarError(f"_cell_date: could not parse {text}") from e
-
-    def _cell_time(self, cell: t.Any, required: bool = True) -> datetime.time | None:
-        text = self._cell_text(cell, required=required)
-        if text is None:
-            return None
-        try:
-            return datetime.datetime.strptime(text, "%H:%M %p").time()
-        except ValueError as e:
-            if text.lower() == "canceled":
-                return None
-            raise LegistarError(f"_cell_time: could not parse {text}") from e
+    def _get_table_scraper(
+        self, path: str, expected_headers: list[str], **queryparams
+    ) -> TableScraper:
+        """Perform a GET request and return a TableScraper object."""
+        soup = self._get_soup(path, **queryparams)
+        table_scraper = TableScraper.from_soup(self, soup)
+        if table_scraper.headers != expected_headers:
+            raise LegistarError(f"Unexpected headers: {table_scraper.headers}")
+        return table_scraper
 
     def get_calendar(self) -> list[CalendarEntry]:
         """Get the calendar."""
-        soup = self._get_soup("/Calendar.aspx")
-        table = soup.find("table", class_="rgMasterTable")
-        if not isinstance(table, Tag):
-            raise LegistarError("get_calendar: could not find calendar table")
-        headers = table.find_all("th", class_="rgHeader")
-        columns = [
-            header.text.encode("ascii", errors="ignore")
-            .decode("ascii")
-            .lower()
-            .replace(":", "")
-            .strip()
-            for header in headers
-        ]
-        if columns != CALENDAR_EXPECTED_COLUMNS:
-            # FUTURE: for now, just raise an error; in the future, we may
-            # want to support other Legistar customers & column names.
-            raise LegistarError(
-                f"get_calendar: expected: {CALENDAR_EXPECTED_COLUMNS}, got {columns}"
-            )
+        table_scraper = self._get_table_scraper("/Calendar.aspx", CALENDAR_HEADERS)
 
-        def make_calendar_entry(row) -> CalendarEntry:
-            cells = row.find_all("td")
-            body, body_url = self._cell_link(cells[CalendarColumn.NAME])
-            assert body is not None
-            assert body_url is not None
-            date = self._cell_date(cells[CalendarColumn.MEETING_DATE])
-            assert date is not None
-            time = self._cell_time(cells[CalendarColumn.MEETING_TIME], required=False)
-            location = self._cell_text(cells[CalendarColumn.MEETING_LOCATION])
-            assert location is not None
-            _, details_url = self._cell_link(cells[CalendarColumn.MEETING_DETAILS])
-            assert details_url is not None
-            _, agenda_url = self._cell_link(cells[CalendarColumn.AGENDA])
-            assert agenda_url is not None
-            _, agenda_packet_url = self._cell_link(
-                cells[CalendarColumn.AGENDA_PACKET], required=False
-            )
-            _, minutes_url = self._cell_link(
-                cells[CalendarColumn.MINUTES], required=False
-            )
-            _, video_url = self._cell_link(
-                cells[CalendarColumn.SEATTLE_CHANNEL], required=False
-            )
+        def _make_entry(row: RowScraper) -> CalendarEntry:
+            body, body_url = row.get_link_and_text("name")
+            date = row.get_date("meeting date")
+            time = row.get_optional_time("meeting time")
+            location = row.get_text("meeting location")
+            _, details_url = row.get_link_and_text("meeting details")
+            _, agenda_url = row.get_link_and_text("agenda")
+            _, agenda_packet_url = row.get_optional_link_and_text("agenda packet")
+            _, minutes_url = row.get_optional_link_and_text("minutes")
+            _, video_url = row.get_optional_link_and_text("seattle channel")
+
             return CalendarEntry(
                 body=body,
                 body_url=body_url,
@@ -179,6 +260,4 @@ class LegistarScraper:
                 video_url=video_url,
             )
 
-        rows = table.find_all("tr", class_="rgRow")
-        entries = [make_calendar_entry(row) for row in rows]
-        return entries
+        return [_make_entry(row) for row in table_scraper]
