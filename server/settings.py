6cf993b8ba5e52e59304609d4e69a2ea2b4c7b80
Dave <davepeck@gmail.com>
2023-04-28 18:17:36 -0700

    Bugfix time zone setting

--- server/settings.py
+++ server/settings.py
@@ -99,7 +99,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # --------------------------------------------------------------------
 
 LANGUAGE_CODE = "en-us"
-TIME_ZONE = "UTC"
+TIME_ZONE = "America/Los_Angeles"
 USE_I18N = True
 USE_TZ = True
 
